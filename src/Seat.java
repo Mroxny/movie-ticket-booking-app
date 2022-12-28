5ed5ecb06f9e5374a63e50598dfdd5d33d1f65d4
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-28 19:37:39 +0100

    Update Seat.java

--- src/Seat.java
+++ src/Seat.java
@@ -1,2 +1,36 @@
 public class Seat {
+    private int row;
+    private int column;
+    private TicketType ticketType;
+
+    public Seat(int row, int column, TicketType ticketType) {
+        this.row = row;
+        this.column = column;
+        this.ticketType = ticketType;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public TicketType getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(TicketType ticketType) {
+        this.ticketType = ticketType;
+    }
 }
+
