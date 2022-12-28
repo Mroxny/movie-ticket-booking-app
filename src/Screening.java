82f1af327472a19e01479bd9947a71f9efe777b6
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-28 19:46:45 +0100

    Update Screening.java

--- src/Screening.java
+++ src/Screening.java
@@ -6,12 +6,14 @@ public class Screening {
     private LocalTime startTime;
     private LocalTime endTime;
     private List<Seat> seats;
+    private Movie movie;
 
-    public Screening(int screeningRoom, LocalTime startTime, LocalTime endTime, List<Seat> seats) {
+    public Screening(int screeningRoom, LocalTime startTime, LocalTime endTime, List<Seat> seats, Movie movie) {
         this.screeningRoom = screeningRoom;
         this.startTime = startTime;
         this.endTime = endTime;
         this.seats = seats;
+        this.movie = movie;
     }
 
     public int getScreeningRoom() {
@@ -44,5 +46,13 @@ public class Screening {
 
     public void setSeats(List<Seat> seats) {
         this.seats = seats;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
     }
 }
