fde3287b24b694b0567e97f8ea4828cb58783575
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-28 19:35:26 +0100

    Update Screening.java

--- src/Screening.java
+++ src/Screening.java
@@ -1,2 +1,39 @@
+import java.time.LocalTime;
+import java.util.List;
+
 public class Screening {
+    private int screeningRoom;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public Screening(int screeningRoom, LocalTime startTime, LocalTime endTime) {
+        this.screeningRoom = screeningRoom;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public int getScreeningRoom() {
+        return screeningRoom;
+    }
+
+    public void setScreeningRoom(int screeningRoom) {
+        this.screeningRoom = screeningRoom;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+    
 }
