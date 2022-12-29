5232431bfeeb8094d230b572ce4570661a1911c6
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-29 17:18:24 +0100

    Update Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -1,8 +1,10 @@
 public class Movie {
     private String title;
+    private int length;
 
-    public Movie(String title) {
+    public Movie(String title, int length) {
         this.title = title;
+        this.length = length;
     }
 
     public String getTitle() {
@@ -11,5 +13,13 @@ public class Movie {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
     }
 }
