5f427eec5b232aa1023b9e7708bec08872ca0b1d
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-29 17:08:32 +0100

    Update MultiplexManager.java

--- src/MultiplexManager.java
+++ src/MultiplexManager.java
@@ -1,2 +1,35 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MultiplexManager {
+    private List<Movie> movies;
+    private List<Screening> screenings;
+    private List<Reservation> reservations;
+
+    public MultiplexManager(List<Screening> screenings) {
+        this.screenings = screenings;
+        movies = new ArrayList<>();
+        reservations = new ArrayList<>();
+    }
+
+    public List<Movie> listMovies(LocalDate day, LocalTime startTime, LocalTime endTime) {
+        return movies;
+    }
+
+
+    public Reservation makeReservation(String name, String surname, Screening screening, List<Seat> seats) {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime expirationTime = now.plusMinutes(15);
+        double totalAmount = seats.stream().mapToDouble(seat -> seat.getTicketType().getPrice()).sum();
+        Reservation reservation = new Reservation(name, surname, screening, seats, expirationTime, totalAmount);
+        reservations.add(reservation);
+        return reservation;
+    }
 }
+
