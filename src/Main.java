d4b9c765878038a566995678e0acd3c1a7b54307
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-29 17:28:21 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
         LocalTime testTime = LocalTime.of(13,15);
         System.out.println(testTime);
+
+        LocalDate testDate = LocalDate.of(2001,2,22);
+        System.out.println(testDate);
     }
 }
