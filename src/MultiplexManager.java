9f7f61cbbe7a50073b703f19fe20a2d79fd1932d
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-29 19:01:31 +0100

    Update MultiplexManager.java

--- src/MultiplexManager.java
+++ src/MultiplexManager.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,8 +17,14 @@ public class MultiplexManager {
         reservations = new ArrayList<>();
     }
 
-    public List<Movie> listMovies(LocalDate day, LocalTime startTime, LocalTime endTime) {
-        return movies;
+    public List<Screening> listScreenings(LocalDate day, LocalTime startTime, LocalTime endTime) {
+        return screenings.stream()
+                .filter(screening -> screening.getDay().isEqual(day) &&
+                        screening.getStartTime().isAfter(startTime) &&
+                        screening.getStartTime().isBefore(endTime))
+                .sorted(Comparator.comparing((Screening screening) -> screening.getMovie().getTitle())
+                        .thenComparing(Screening::getStartTime))
+                .collect(Collectors.toList());
     }
 
 
