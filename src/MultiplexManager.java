f9175634a33212b6e62c7967a2105ea971c662e1
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-29 19:14:18 +0100

    Update MultiplexManager.java

--- src/MultiplexManager.java
+++ src/MultiplexManager.java
@@ -1,19 +1,18 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.chrono.ChronoLocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MultiplexManager {
-    private List<Movie> movies;
     private List<Screening> screenings;
     private List<Reservation> reservations;
 
     public MultiplexManager(List<Screening> screenings) {
         this.screenings = screenings;
-        movies = new ArrayList<>();
         reservations = new ArrayList<>();
     }
 
@@ -27,10 +26,34 @@ public class MultiplexManager {
                 .collect(Collectors.toList());
     }
 
+    public Screening getScreening(int screeningId) {
+        for (Screening s : screenings) {
+            if (s.getScreeningId() == screeningId) return s;
+        }
+        return null;
+    }
 
-    public Reservation makeReservation(String name, String surname, Screening screening, List<Seat> seats) {
+    public Reservation makeReservation(String name, String surname, int screeningId, List<Seat> seats) {
+        Screening screening = getScreening(screeningId);
+
+        if (screening == null) {
+            return null;
+        }
 
         LocalDateTime now = LocalDateTime.now();
+        if (now.isAfter(ChronoLocalDateTime.from(screening.getStartTime())) || now.plusMinutes(15).isAfter(ChronoLocalDateTime.from(screening.getStartTime()))) {
+            return null;
+        }
+
+        if (seats.size() < 1 || seats.size() > 10) {
+            return null;
+        }
+
+        for (Seat seat : seats) {
+            if (!screening.getSeats().contains(seat)) {
+                return null;
+            }
+        }
 
         LocalDateTime expirationTime = now.plusMinutes(15);
         double totalAmount = seats.stream().mapToDouble(seat -> seat.getSeatType().getPrice()).sum();
