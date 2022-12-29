9903d0f893c74d7088f12af1f32b0dd0c80664ce
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-29 17:57:21 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        LocalTime testTime = LocalTime.of(13,15);
-        System.out.println(testTime);
+        LocalTime testTime1_1 = LocalTime.of(13,15);
+        System.out.println(testTime1_1);
+        LocalTime testTime1_2 = LocalTime.of(13,15);
+        System.out.println(testTime1_2);
+        LocalTime testTime2_1 = LocalTime.of(13,15);
+        System.out.println(testTime2_1);
+        LocalTime testTime2_2 = LocalTime.of(13,15);
+        System.out.println(testTime2_2);
 
-        LocalDate testDate = LocalDate.of(2001,2,22);
-        System.out.println(testDate);
+        LocalDate testDate1 = LocalDate.of(2001,2,22);
+        System.out.println(testDate1);
+        LocalDate testDate2 = LocalDate.now();
+        System.out.println(testDate1);
+
+        List<Seat> seats1 = Arrays.asList(
+                new Seat(1, 1, TicketType.ADULT),
+                new Seat(1, 2, TicketType.ADULT),
+                new Seat(1, 3, TicketType.ADULT),
+                new Seat(1, 4, TicketType.ADULT),
+                new Seat(1, 5, TicketType.ADULT)
+        );
     }
 }
