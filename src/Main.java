633f53c294a0f2190b7c9ae7b93d91b1a7ba5c3c
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-29 17:24:19 +0100

    Test time

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
-        
+        LocalTime testTime = LocalTime.of(13,15);
+        System.out.println(testTime);
     }
 }
