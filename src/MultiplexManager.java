00099c6e7039ed515a18e7e997d26a4d3abf1fec
Mroxny <Maciej.h.mrozek@gmail.com>
2022-12-29 19:25:50 +0100

    Update MultiplexManager.java

--- src/MultiplexManager.java
+++ src/MultiplexManager.java
@@ -19,8 +19,8 @@ public class MultiplexManager {
     public List<Screening> listScreenings(LocalDate day, LocalTime startTime, LocalTime endTime) {
         return screenings.stream()
                 .filter(screening -> screening.getDay().isEqual(day) &&
-                        screening.getStartTime().isAfter(startTime) &&
-                        screening.getStartTime().isBefore(endTime))
+                        screening.getStartTime().minusMinutes(1).isAfter(startTime) &&
+                        screening.getStartTime().minusMinutes(1).isBefore(endTime))
                 .sorted(Comparator.comparing((Screening screening) -> screening.getMovie().getTitle())
                         .thenComparing(Screening::getStartTime))
                 .collect(Collectors.toList());
