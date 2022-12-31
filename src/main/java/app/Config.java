26acf8e5a15f95a6435e6686d297e9207f865cc5
Mroxny <50219282+Mroxny@users.noreply.github.com>
2022-12-31 16:28:13 +0100

    Update Config.java

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -1,9 +1,11 @@
 package app;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 @Configuration
+@ComponentScan("app")
 public class Config extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
